043301c17086a39d1b74485d45a2a5aa03a872ed
Ryan Seddon <seddon.ryan@gmail.com>
2014-05-18 13:23:45 +1000

    Include built library

---
+++ index.js
@@ -0,0 +1,38 @@
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Frame=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+/** @jsx React.DOM */
+var Frame = React.createClass({displayName: 'Frame',
+  render: function() {
+    return this.transferPropsTo(React.DOM.iframe(null ));
+  },
+  componentDidMount: function() {
+    this.renderFrameContents();
+  },
+  renderFrameContents: function() {
+    var doc = this.getDOMNode().contentDocument;
+    if(doc && doc.readyState === 'complete') {
+      var contents = (
+        React.DOM.div(null, 
+          this.props.head,
+          this.props.children
+        )
+      );
+
+      React.renderComponent(contents, doc.body);
+    } else {
+       setTimeout(this.renderFrameContents, 0);
+    }
+  },
+  componentDidUpdate: function() {
+    this.renderFrameContents();
+  },
+  componentWillUnmount: function() {
+    React.unmountComponentAtNode(this.getDOMNode().contentDocument.body);
+  }
+});
+
+module.exports = Frame;
+
+
+},{}]},{},[1])
+(1)
+});
