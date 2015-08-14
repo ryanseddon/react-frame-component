64dd21908207a58d22e55a60b084b8cfad68db14
Taner Topal <tanertopal42@gmail.com>
2015-08-14 14:44:05 +0200

    added eslint and made sure children are not directly passed to the iframe as props as this results in an error.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default class Frame extends React.Component {
+  static propTypes = {
+    style: React.PropTypes.object,
+    head: React.PropTypes.node
+  }
 
   constructor(props, context) {
     super(props, context);
@@ -22,7 +26,7 @@ export default class Frame extends React.Component {
 
   renderFrameContents() {
     var doc = React.findDOMNode(this).contentDocument;
-    if(doc && doc.readyState === 'complete') {
+    if (doc && doc.readyState === 'complete') {
       var contents = React.createElement('div',
         undefined,
         this.props.head,
@@ -36,14 +40,10 @@ export default class Frame extends React.Component {
   }
 
   render() {
+    const props = {...this.props, children: undefined};
     return (
-      <iframe {...this.props} />
+      <iframe {...props} />
     );
   }
 
-};
-
-Frame.propTypes = {
-  style: React.PropTypes.object,
-  head:  React.PropTypes.node
 };
