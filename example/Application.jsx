ae89557cb03a119d4cafff8002f6f101763c28b9
Jiaming <hfter789@gmail.com>
2018-01-17 10:08:28 -0800

    update example

--- example/Application.jsx
+++ example/Application.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import Frame from '../src';
 
-const InnerFrame = (props, context) => {
-  console.log('IFrame Window and Document: ', context.window, context.document);
-  return (
-    <h1>Inside Frame Text</h1>
-  );
-};
-InnerFrame.contextTypes = {
-  window: PropTypes.any,
-  document: PropTypes.any
+var styles = {
+  border: '1px solid',
+  width: '100%',
+  height: '100%'
 };
 
-const Application = () => (
+const Header = ({ children }) => <h1>{this.props.children}</h1>;
+
+const Content = ({ children }) => <section>{this.props.children}</section>
+
+const App = () => (
   <div>
-    <h1>Outside Frame Text</h1>
-    <Frame>
-      <InnerFrame />
-    </Frame>
+    <Header>Frame example of wrapping application</Header>
+    <Content>
+      <h2>This whole app is wrapped inside an iFrame</h2>
+    </Content>
   </div>
 );
 
-export default Application;
+ReactDOM.render(<Frame style={styles}><App /></Frame>, document.querySelector('#example1'));
+
+const Foobar = () => (
+  <Frame style={styles} head={
+    <style>{'*{color:red}'}</style>
+  }>
+    <h1>Frame example of wrapping component</h1>
+    <p>This is also showing encapuslated styles. All text is red inside this component.</p>
+  </Frame>
+);
+
+ReactDOM.render(<Foobar />, document.querySelector('#example2'));
