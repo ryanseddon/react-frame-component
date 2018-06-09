fe1878a54b9e1bf40ae01bd1fbb738f9ba1702c9
Chris Trevino <chtrevin@microsoft.com>
2018-06-08 18:40:53 -0700

    Check global document and window variables for existence to fix SSR failure

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
+let doc = undefined
+let win = undefined
+if (typeof document !== 'undefined') {
+  doc = document
+}
+if (typeof window !== 'undefined') {
+  win = window
+}
+
 export const {
   Provider: FrameContextProvider,
   Consumer: FrameContextConsumer
-} = React.createContext({ document, window });
+} = React.createContext({ document: doc, window: win });
