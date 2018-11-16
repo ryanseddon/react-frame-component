e4e6d865d559ba9fa0622bded402d09b70b3ba25
Grzegorz Dominiczak <grzegorz@codehard.pl>
2018-11-16 14:13:22 +0100

    Add tests for accessing context via contextType

--- test/Context.spec.jsx
+++ test/Context.spec.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import { expect } from 'chai';
-import { FrameContextProvider, FrameContextConsumer } from '../src/Context';
+import {
+  FrameContextProvider,
+  FrameContextConsumer,
+  FrameContext
+} from '../src/Context';
 
 describe('The DocumentContext Component', () => {
   it('will establish context variables', done => {
@@ -18,6 +22,30 @@ describe('The DocumentContext Component', () => {
         }}
       </FrameContextConsumer>
     );
+    ReactTestUtils.renderIntoDocument(
+      <FrameContextProvider value={{ document, window }}>
+        <Child />
+      </FrameContextProvider>
+    );
+  });
+
+  it('exports full context instance to allow accessing via Class.contextType', done => {
+    const document = { foo: 1 };
+    const window = { bar: 2 };
+
+    class Child extends React.Component {
+      componentDidMount() {
+        const { document: doc, window: win } = this.context;
+        expect(doc).to.deep.equal({ foo: 1 });
+        expect(win).to.deep.equal({ bar: 2 });
+        done();
+      }
+      render() {
+        return null;
+      }
+    }
+    Child.contextType = FrameContext;
+
     ReactTestUtils.renderIntoDocument(
       <FrameContextProvider value={{ document, window }}>
         <Child />
