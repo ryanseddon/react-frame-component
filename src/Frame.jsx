10439d50f21daf43721c74fed583af3f66f1bebb
Chris Weed <chris@cjweed.com>
2019-07-21 18:10:20 -0500

    fix: Null check for doc (#142)

--- src/Frame.jsx
+++ src/Frame.jsx
@@ -56,7 +56,7 @@ export default class Frame extends Component {
   }
 
   getDoc() {
-    return this.node.contentDocument; // eslint-disable-line
+    return this.node ? this.node.contentDocument : null; // eslint-disable-line
   }
 
   getMountTarget() {
@@ -77,6 +77,10 @@ export default class Frame extends Component {
     }
 
     const doc = this.getDoc();
+
+    if (!doc) {
+      return null;
+    }
 
     const contentDidMount = this.props.contentDidMount;
     const contentDidUpdate = this.props.contentDidUpdate;
