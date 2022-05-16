9c979973a7bdccf8644560e80ea5424754828955
Nikita Seleckis <nikita@seleckis.lv>
2022-05-16 11:11:51 +0300

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,31 @@
-declare module 'react-frame-component';
+declare module 'react-frame-component' {
+  import * as React from 'react';
+
+  export interface FrameComponentProps
+    extends React.IframeHTMLAttributes<HTMLIFrameElement> {
+    head?: React.ReactNode | undefined;
+    mountTarget?: string | undefined;
+    initialContent?: string | undefined;
+    contentDidMount?: (() => void) | undefined;
+    contentDidUpdate?: (() => void) | undefined;
+    children: React.ReactNode;
+    ref?: React.Ref<FrameComponent>;
+  }
+
+  export default class FrameComponent extends React.Component<
+    FrameComponentProps
+  > {}
+
+  export interface FrameContextProps {
+    document?: HTMLDocument;
+    window?: Window;
+  }
+
+  export const FrameContext: React.Context<FrameContextProps>;
+
+  export const FrameContextProvider: React.Provider<FrameContextProps>;
+
+  export const FrameContextConsumer: React.Consumer<FrameContextProps>;
+
+  export function useFrame(): FrameContextProps;
+}
