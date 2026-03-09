9fc811c1318b241253795bf3727e6c7626459098
Ryan Seddon <seddon.ryan@gmail.com>
2026-03-09 14:43:16 +1100

    feat: add auto deploy of example page

---
+++ vite.example.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: resolve(__dirname, 'example'),
+  plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'example-dist'),
+    emptyOutDir: true
+  }
+});
