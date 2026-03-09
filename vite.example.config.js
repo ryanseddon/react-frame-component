fec48537a6d5bdb5277867cd5190a899c6df01bc
Ryan Seddon <seddon.ryan@gmail.com>
2026-03-09 14:54:00 +1100

    Correct assets path (#276)
    * Correct assets path
    
    * fix: avoid double ci run

--- vite.example.config.js
+++ vite.example.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: resolve(__dirname, 'example'),
+  base: './',
   plugins: [react()],
   build: {
     outDir: resolve(__dirname, 'example-dist'),
